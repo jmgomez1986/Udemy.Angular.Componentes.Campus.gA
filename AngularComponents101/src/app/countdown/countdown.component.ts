34613cd80daf464dd344775dd9788312b1e6a381
jmgomez2018 <jmgametal@gmail.com>
2019-10-04 14:05:08 -0300

    Se implementa progressBar.component y countdown.component

--- AngularComponents101/src/app/countdown/countdown.component.ts
+++ AngularComponents101/src/app/countdown/countdown.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
+export class CountdownComponent implements OnInit {
+
+  @Input() init: number = null;
+  public counter: number = 0;
 
   constructor() { }
+
+  ngOnInit() {
+    this.startCoutdown();
+  }
+
+  startCoutdown() {
+    if (this.init && this.init > 0) {
+      this.counter = this.init;
+      this.doCountdown();
+    }
+  }
+
+  doCountdown() {
+     setTimeout(() => {
+       this.counter = this.counter - 1;
+       this.processCount();
+     }, 1000);
+  }
+
+  processCount() {
+    // Emmit Event COUNT
+    console.log('Count is: ', this.counter);
+
+    if (this.counter === 0) {
+      // Emmit Event COUNT END
+      console.log('Counter END');
+    } else {
+      this.doCountdown();
+    }
+  }
 
 }
